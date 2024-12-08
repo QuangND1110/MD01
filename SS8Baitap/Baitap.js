342d017f306fa993b6e71e5ac2b87578298d6991
QuangND1110 <nguyenquang.111099@gmail.com>
2024-12-08 13:35:45 +0900

    Add files via upload

---
+++ SS8Baitap/Baitap.js
@@ -0,0 +1,68 @@
+// Bai1
+// alert("xin chao")
+
+
+// Bai2
+// let i = 10;
+// let f = 20.5;
+// let b = true;
+// let s = "Ha Noi"
+// console.log(i);
+// console.log(f);
+// console.log(b?"dung":"sai");
+// console.log(s);
+
+// Bai3
+// let width = 10;
+// let height = 20;
+// document.write("Dien tich hcn: ", width*height);
+
+// Bai4
+// confirm("Ban du 18 tuoi chua?");
+
+// Bai5
+// let a = +prompt("nhap diem Vat Li:" )
+// let d = +prompt("nhap diem Hoa Hoc:" )
+// let c = +prompt("nhap diem Sinh Hoc:" )
+// let DTB = (a+d+c)/3;
+// console.log("Diem trung binh= ", DTB);
+
+// Bai6
+// let input = +prompt("Nhap do C:");
+// let F = input*9/5+32;
+// console.log(input,`do C=`, F);
+
+// Bai7
+// let a = +prompt("Nhap chieu dai hcn:")
+// let b = +prompt("Nhap chieu rong hcn:")
+// let cv = (a+b)*2;
+// let dt = a*b;
+// console.log("chu vi hcn=", cv);
+// console.log("dien tich hcn=", dt);
+
+// Bai8
+// const PI = 3.14;
+// let r = +prompt("ban kinh ht:");
+// let cv = 2*PI*r;
+// let dt = Math.pow(cv,2)/(4*PI);
+// console.log("chu vi ht=", cv);
+// console.log("dien tich ht=", dt);
+
+// // Bai9
+// let a = 5;
+// let b = "5";
+// console.log(a==b);  
+// // dấu "==" chỉ so sánh giá trị nên true 5=5
+// console.log(a===b);
+// // dấu "===" so sánh kiểu dữ liệu nên fasle number#string
+// console.log(a!=b);  
+// // dấu != nghĩa là "giá trị a không bằng giá trị b" nên false
+// console.log(a!==b);
+// // dấu !== nghĩa là "kiểu dữ liệu a khác kiểu dữ liêu b" nên true
+
+// Bai10
+let a = 6;
+let result = ++a + a + a++ + ++a + a;
+// let result = 7 + 7 + 7 + 9 + 9
+console.log("Tong=", result);
+
